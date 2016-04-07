fa880a52182e0fbd8eca23d30747241093a431f5
Ross Pace <rpace@avantia-inc.com>
2016-04-07 12:46:49 -0400

    adding more documentation on helper functions

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ module.exports = {
     next: next
 };
 
+/**
+ * Helper function for handling errors in a worker process; Will exit the worker process in an error exists
+ * @method
+ * @param {Error|string} error object
+ * @param {string|object} details to be reported with error
+ * @param {function} [callback] callback will be passed both error and details
+ */
 function handleError(err, details, callback) {
     if (err) {
         if (!details) details = 'BatchJob received error:';
@@ -16,4 +23,8 @@ function handleError(err, details, callback) {
     }
 };
 
+/**
+ * Helper function used by a worker process to call for the next batch item
+ * @method
+ */
 function next() { process.send('next'); }
